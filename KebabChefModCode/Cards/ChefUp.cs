372a608ffdc536fb56c6673a01cfb2c01f4f6046
stefanspitse4pm <stefan.spitse@student.nhlstenden.com>
2026-06-20 21:27:54 +0200

    added Chef Up!

---
+++ KebabChefModCode/Cards/ChefUp.cs
@@ -0,0 +1,29 @@
+using KebabChefMod.KebabChefModCode.Cards;
+using KebabChefMod.KebabChefModCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace KebabChefMod.KebabChefModCode.Cards;
+
+public class ChefUp() : KebabChefModCard(1,
+    CardType.Skill, CardRarity.Basic,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<SkewerPower>(3M)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        ChefUp chefUp = this;
+        SkewerPower skewerPower = await PowerCmd.Apply<SkewerPower>(choiceContext, chefUp.Owner.Creature, chefUp.DynamicVars["SkewerPower"].BaseValue, chefUp.Owner.Creature, (CardModel)null);
+    }
+
+    protected override void OnUpgrade()
+    {
+
+    }
+}
